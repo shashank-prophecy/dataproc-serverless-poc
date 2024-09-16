4761d0c83b0e98bb42aa9b3f606f5adb951c37a0
shashank@prophecy.io <shashank@prophecy.io>
2024-09-16 15:54:56 +0000

    Updates customer_write output path to Google Cloud Storage

--- pipelines/customer_test/code/customer_test/graph/customer_write.py
+++ pipelines/customer_test/code/customer_test/graph/customer_write.py
@@ -13,4 +13,4 @@ def customer_write(spark: SparkSession, in0: DataFrame):
         .mode("overwrite")\
         .option("separator", ",")\
         .option("header", True)\
-        .csv("dbfs:/FileStore/data_engg/shashank/customer/customer_output.csv")
+        .csv("gs://bigquery-temp-demo/final_data/customer_output.csv")
